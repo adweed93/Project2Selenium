cebe8159e39134a625e32fdcf478d19ff4205880
John Eifert <johnreifert94@gmail.com>
2018-05-09 16:51:57 -0400

    brought this repo up to date

--- src/test/java/com/revature/testing/TestNGTests.java
+++ src/test/java/com/revature/testing/TestNGTests.java
@@ -1,72 +1,83 @@
 //John Eifert
 package com.revature.testing;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import java.util.ArrayList;
 
 
 
 public class TestNGTests
 {
-	@BeforeTest
-	public static WebDriver getDriver()
-	{
-		//Multi-OS support:
-		String path = "src/main/resources/chromedriver";
-		File tmpDir = new File(path);
-		boolean exists = tmpDir.exists();
-		if(!exists)		// windows path
-			path = "src\\main\\resources\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver", path);
-		
-		
-		//Make and return a ChromeDriver:
-		return(new ChromeDriver());
-	}
+	//Get a WebDriver object for use in the tests:
+	WebDriver browser = TestingMethods.getDriver();
 	
 	
 	@Test
 	public void trainerLoginTest()
 	{
-		//Get a WebDriver object:
-		WebDriver browser = getDriver();
-		
-		//Navigate to the login page:
-		browser.get("https://dev.assignforce.revaturelabs.com");
-				
-		//Acquire the relevant input objects:
-		WebElement usernameField = browser.findElement(By.id("username"));
-		WebElement passwordField = browser.findElement(By.id("password"));
-		WebElement submitButton = browser.findElement(By.id("Login"));
-		
 		//Perform the login actions:
-		usernameField.sendKeys("test.trainer@revature.com.int1");
-		passwordField.sendKeys("trainer123");
-		submitButton.click();
+		TestingMethods.trainerLogin(browser);
 		
 		//Establish what URL we expect the new URL to be and what the new URL actually is:
 		String expectedURL = "https://dev.assignforce.revaturelabs.com/";
 		String actualURL = browser.getCurrentUrl();
 		
-		/*
-		//Print the new URL:
-		System.out.println("Current URL = " + actualURL);
-		*/
-		
 		//Test whether the new URL is what it should be:
 		Assert.assertEquals(actualURL, expectedURL);
-		
-		//Close the browser window:
-		browser.close();
 	}
+	
+	@Test(dependsOnMethods="trainerLoginTest")
+	public void trainerNavButtonTest()
+	{
+		//Perform the login actions:
+			//Login has already been performed by the previous test.
+		
+		//Test each of the nav buttons:
+		String expectedURL = "";
+		String actualURL = "";
+		
+		ArrayList<String> buttonNames = new ArrayList<String>();
+		buttonNames.add("overview");
+		buttonNames.add("batches");
+		buttonNames.add("locations");
+		buttonNames.add("curricula");
+		buttonNames.add("trainers");
+		buttonNames.add("profile");
+		buttonNames.add("reports");
+		buttonNames.add("settings");
+		
+		for(int i=0; i<buttonNames.size(); i++)
+		{
+			TestingMethods.pushButtonFromNavBar(browser, buttonNames.get(i));
+			expectedURL = "https://dev.assignforce.revaturelabs.com/" + buttonNames.get(i);
+			if(i == 0)
+				expectedURL = "https://dev.assignforce.revaturelabs.com/home";
+			if(i == 3)
+				expectedURL = "https://dev.assignforce.revaturelabs.com/curriculum";
+			actualURL = browser.getCurrentUrl();
+			System.out.println(actualURL);
+			Assert.assertEquals(actualURL, expectedURL);
+		}
+		
+			//Logout button:
+		TestingMethods.pushButtonFromNavBar(browser, "logout");
+		expectedURL = "https://dev.assignforce.revaturelabs.com/reports";
+		actualURL = browser.getCurrentUrl();
+		System.out.println(actualURL);
+		Assert.assertNotEquals(actualURL, expectedURL);
+	}
+	
+	@Test(dependsOnMethods="trainerNavButtonTest")
+	public void homeSortByNameTest()
+	{
+		
+		
+		
+		browser.quit();
+	}
+	
+	
 	
 }
